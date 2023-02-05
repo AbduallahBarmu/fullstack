bc3f468252a00df95e79eceb383a1917da3b42b1
AbduallahBarmu <abduallah.imu2@gmail.com>
2023-02-05 18:56:08 +0300

    jest configration

--- back-store/src/products/controller/products.controller.spec.ts
+++ back-store/src/products/controller/products.controller.spec.ts
@@ -1,18 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from '../controller/products.controller';
+import { ProductsService } from '../services/products.service';
 
 describe('ProductsController', () => {
-  let controller: ProductsController;
+  let productsController: ProductsController;
+  let productsService: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
     }).compile();
 
-    controller = module.get<ProductsController>(ProductsController);
+    productsController = module.get<ProductsController>(ProductsController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(productsController).toBeDefined();
   });
 });
